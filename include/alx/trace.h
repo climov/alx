111f344ffad46c56a0df7c26c310cc7c566808b2
Catalin Climov <climov@gmail.com>
2025-04-05 09:32:51 +0300

    Small fixes for fdbg and ferr.

--- include/alx/trace.h
+++ include/alx/trace.h
@@ -4,8 +4,8 @@
 #include <cstdio>
 
 #ifdef _WIN32
-#define fdbg(FMT, ...) do { std::print(stdout, FMT, __VA_ARGS__); std::cout << "\n" << std::flush; } while (false)
-#define ferr(FMT, ...) do { std::print(stderr, FMT, __VA_ARGS__); std::cerr << "\n" << std::flush; } while (false)
+#define fdbg(FMT, ...) do { std::print(stdout, FMT, __VA_ARGS__); std::fputs(stdout, "\n"); std::fflush(stdout); } while (false)
+#define ferr(FMT, ...) do { std::print(stderr, FMT, __VA_ARGS__); std::fputs(stdeff, "\n"); std::fflush(stderr); } while (false)
 #else
 #define fdbg(FMT, ...) do { std::println(stdout, FMT, ##__VA_ARGS__); std::fflush(stdout); } while (false)
 #define ferr(FMT, ...) do { std::println(stderr, FMT, ##__VA_ARGS__); std::fflush(stderr); } while (false)
